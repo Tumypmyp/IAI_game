878e8d758a61bc165b9c999a3912e35a9ac122aa
Timur <gaynoullin@gmail.com>
2022-03-08 19:47:30 +0300

    hashCode removed

--- src/Point.java
+++ src/Point.java
@@ -2,12 +2,10 @@ import java.util.Objects;
 
 public class Point {
     public int x, y;
-    private final int hashCode;
 
     Point(int x, int y) {
         this.x = x;
         this.y = y;
-        this.hashCode = Objects.hash(x, y);
     }
 
     public static Point add(Point a, Point b) {
@@ -20,20 +18,5 @@ public class Point {
 
     public static Point not(Point a) {
         return new Point(-a.x, -a.y);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        Point p2 = (Point) o;
-        return x == p2.x && y == p2.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return this.hashCode;
     }
 }
