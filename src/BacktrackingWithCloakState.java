3b58046392b34d74676a87966cfbe393f809c225
Timur <gaynoullin@gmail.com>
2022-03-09 17:39:01 +0300

    backtracking with cloak state added

---
+++ src/BacktrackingWithCloakState.java
@@ -0,0 +1,55 @@
+public class BacktrackingWithCloakState implements Strategy {
+    Player player;
+
+    BacktrackingWithCloakState() {
+    }
+
+    @Override
+    public void run(boolean debug) {
+
+        if (debug)
+            player.game.print();
+
+        boolean ok = dfsToCard(Card.BOOK, new boolean[Game.ROWS][Game.COLUMNS][2]);
+
+        if (debug)
+            player.game.print();
+
+        if (!ok)
+            return;
+        dfsToCard(Card.EXIT, new boolean[Game.ROWS][Game.COLUMNS][2]);
+
+        if (debug)
+            player.game.print();
+    }
+
+    @Override
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public boolean ok(Point p) {
+        return 0 <= p.x && p.x < Game.ROWS && 0 <= p.y && p.y < Game.COLUMNS
+                && !player.getVisibleCardsByPoint(p).contains(Card.CAT)
+                && (!player.getVisibleCardsByPoint(p).contains(Card.SEEN) || player.haveCloak);
+    }
+
+    public boolean dfsToCard(Card card, boolean[][][] used) {
+        if (player.getStatus() == Status.LOST)
+            return false;
+        if (player.getVisibleCardsByPoint(player.coordinates).contains(card))
+            return true;
+        used[player.coordinates.x][player.coordinates.y][player.haveCloak ? 1 : 0] = true;
+
+        for (Point move : Player.MOVES) {
+            Point next = Point.add(player.coordinates, move);
+            if (ok(next) && !used[next.x][next.y][player.haveCloak ? 1 : 0]) {
+                player.useMove(move);
+                if (dfsToCard(card, used))
+                    return true;
+                player.useMove(Point.not(move));
+            }
+        }
+        return false;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println(new Game(9994, new Backtracking()).run(true));
+
+        testStrategy(new Backtracking());
+        testStrategy(new BacktrackingWithCloakState());
+    }
+
+    static void testStrategy(Strategy strategy) {
         Map<Status, List<Integer>> statistic = new HashMap<>();
-        for (int i = 1; i < 100; i++) {
-            Game game = new Game(i, new Backtracking());
-            System.out.println(i + ": " + game.run(false));
+        for (int i = 1; i < 1000000; i++) {
+            Game game = new Game(i, strategy);
+            game.run(false);
+//            System.out.println(i + ": " + game.run(false));
             if (statistic.get(game.status) == null)
                 statistic.put(game.status, new ArrayList<>());
             statistic.get(game.status).add(game.player.timer);
@@ -14,7 +21,7 @@ public class Main {
         for (Map.Entry<Status, List<Integer>> entry : statistic.entrySet()) {
             OptionalDouble average = entry.getValue().stream().mapToDouble(a -> a).average();
             double avg = average.isPresent() ? average.getAsDouble() : 0;
-            System.out.println(entry.getKey() + ":" + avg + " " + entry.getValue().toString());
+            System.out.println(entry.getKey() + ":" + avg + " " + entry.getValue().size());
         }
     }
 }
